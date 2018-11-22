e22879ea48c88b59719ec7d8fac0f7e3ac48bc30
9986189750 <shishirpatil007@gmail.com>
2018-11-22 18:42:05 +0530

    efr

--- getLocation.php
+++ getLocation.php
@@ -1,46 +1,21 @@
-
 <?php
-
-echo "here"; 
-if(isset($_GET["Latitude"]))
-{ $Latitude=$_GET["Latitude"];	}
-
-if(isset($_GET["Longitude"]))
-{ $Longitude=$_GET["Longitude"];	}
-
-$IPaddress=$_SERVER['REMOTE_ADDR'];
-$msg='';
-
-
-
-//$geoIP  = json_decode(file_get_contents("http://freegeoip.net/json/$IPaddress"), true);
-
-//echo '<pre>'; print_r($geoIP); exit;
-
-$msg.="IPaddress: ".$IPaddress;
-$msg.='lat: ' . $Latitude . '<br />';
-$msg.='lat: ' . $Longitude . '<br />';
-
-
-$headers = "From: soumyachidambara@gmail.com\r\n";
-$headers .= "Reply-To: shishirpatil007@gmail.com\r\n";
-$headers .= "Return-Path: soumyachidambara@gmail.com\r\n";
-
-$to="shishirpatil007@gmail.com";
-$subject="Location";
-
-if ( mail($to,$subject,$msg,$headers) ) {
-   echo "The email has been sent!";
-   } else {
-   echo "The email has failed!";
-   }
-
-
-
-echo json_encode($msg);
-exit;
-
-
-
-
+//if latitude and longitude are submitted
+if(!empty($_POST['latitude']) && !empty($_POST['longitude'])){
+    //send request and receive json data by latitude and longitude
+    $url = 'http://maps.googleapis.com/maps/api/geocode/json?latlng='.trim($_POST['latitude']).','.trim($_POST['longitude']).'&sensor=false';
+    $json = @file_get_contents($url);
+    $data = json_decode($json);
+    $status = $data->status;
+    
+    //if request status is successful
+    if($status == "OK"){
+        //get address from json data
+        $location = $data->results[0]->formatted_address;
+    }else{
+        $location =  '';
+    }
+    
+    //return address to ajax 
+    echo $location;
+}
 ?>
